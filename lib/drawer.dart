5cc55328753df5d6c7ee38aca0a494bef5021b68
aden septian <adenseptian52@gmail.com>
2024-11-09 08:32:53 +0700

    Update fitur terbaru

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -10,38 +10,42 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           '',
-          style: TextStyle(
-            color: Colors.white,
-          ),
+          style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.blue[900],
-        foregroundColor: Colors.white, //for universal colors
+        foregroundColor: Colors.white, // universal color setting
       ),
       drawer: buildGroupDrawer(context),
-      body: GridView.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10.5,
-          mainAxisSpacing: 10.0,
-          shrinkWrap: true,
-          children: List.generate(10, (index) {
-            return Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Container(
-                  height: 5.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                    color: Colors.blue[200],
-                  ),
-                  child: Text(
-                    'Materi $index',
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ));
-          })),
+      body: Center(
+        child: Text('Pemrograman mobile $title'),
+      ),
+      // Uncomment the below section to use GridView instead of Center widget
+      // body: GridView.count(
+      //   crossAxisCount: 2,
+      //   crossAxisSpacing: 10.5,
+      //   mainAxisSpacing: 10.0,
+      //   shrinkWrap: true,
+      //   children: List.generate(10, (index) {
+      //     return Padding(
+      //       padding: const EdgeInsets.all(10.0),
+      //       child: Container(
+      //         height: 5.0,
+      //         decoration: BoxDecoration(
+      //           borderRadius: BorderRadius.circular(20.0),
+      //           color: Colors.blue[200],
+      //         ),
+      //         child: Text(
+      //           'Materi $index',
+      //           textAlign: TextAlign.center,
+      //           style: const TextStyle(
+      //             color: Colors.black,
+      //             fontSize: 20.0,
+      //           ),
+      //         ),
+      //       ),
+      //     );
+      //   }),
+      // ),
     );
   }
 
@@ -49,53 +53,40 @@ class MyHomePage extends StatelessWidget {
     return Drawer(
       child: ListView(
         children: <Widget>[
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue[900],
+          UserAccountsDrawerHeader(
+            decoration: BoxDecoration(color: Colors.blue[900]),
+            accountName: const Text(
+              'Aden Septian Sasmito',
+              style: TextStyle(fontSize: 10.0),
             ),
-            child: const Text(
-              'aden septian',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
+            accountEmail: const Text('adenseptian52@gmail.com'),
+            currentAccountPictureSize: const Size.square(40),
+            currentAccountPicture: const CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Text(
+                'A',
+                style: TextStyle(fontSize: 25, color: Colors.blue),
               ),
             ),
           ),
-          ExpansionTile(
-            title: const Text('Materi'),
-            children: <Widget>[
-              ListTile(
-                title: const Text('Materi 1'),
-                onTap: () {
-                  Navigator.pop(context); // Close drawer when tapped
-                },
-              ),
-              ListTile(
-                title: const Text('Materi 2'),
-                onTap: () {
-                  Navigator.pop(context); // Close drawer when tapped
-                },
-              ),
-              ListTile(
-                title: const Text('Materi 3'),
-                onTap: () {
-                  Navigator.pop(context); // Close drawer when tapped
-                },
-              ),
-              ListTile(
-                title: const Text('Materi 4'),
-                onTap: () {
-                  Navigator.pop(context); // Close drawer when tapped
-                },
-              ),
-              ListTile(
-                title: const Text('Materi 5'),
-                onTap: () {
-                  Navigator.pop(context); // Close drawer when tapped
-                },
-              ),
-            ],
-          ),
+          // Uncomment the below section for expandable list of materials
+          // ExpansionTile(
+          //   title: const Text('Materi'),
+          //   children: <Widget>[
+          //     for (var i = 1; i <= 5; i++)
+          //       ListTile(
+          //         title: Text('Materi $i'),
+          //         onTap: () {
+          //           Navigator.pop(context); // Close drawer when tapped
+          //         },
+          //       ),
+          //   ],
+          // ),
+          ListTile(leading: Icon(Icons.person),
+          title: Text('aden'),
+          subtitle: Text('XI RPL 2'),
+          trailing: Icon(Icons.chevron_right_rounded),
+          )
         ],
       ),
     );
